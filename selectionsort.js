882735e477473caebc0eae5adfd4300470033382
Gabriel Pereira <adm1nkash1m1r@gmail.com>
2023-01-23 15:08:31 -0300

    algoritmos3

--- selectionsort.js
+++ selectionsort.js
@@ -1,6 +1,17 @@
+const menorValor = require("./algoritmos");
 const menor = require("./algoritmos");
 const livros = require("./livros");
 
 for(let i = 0; i < livros.length; i++){
-    
+    let menor = menorValor(livros, i);
+
+    let livroAtual = livros[i];
+    console.log("livro atual", livros[i]);
+    let livroMenorPreco = livros[menor]
+    console.log("livro menor preço", livros[menor]);
+
+    livros[i] = livroMenorPreco;
+    livros[menor] = livroAtual;
 }
+
+console.log(livros);
