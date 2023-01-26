9ff31f7595795dafa69d0c44361b239814183d91
Gabriel Pereira <adm1nkash1m1r@gmail.com>
2023-01-26 16:26:25 -0300

    insection

---
+++ insectionsort.js
@@ -0,0 +1,18 @@
+const livros = require("./livros");
+
+function insertionSort(lista) {
+    for(atual = 0; atual < lista.length; atual++) {
+        let analise = atual;
+        while(analise > 0 && lista[analise].preco < lista[analise -1].preco){
+            let itemAnalise = lista[analise];
+            let itemAnterior = lista[analise - 1];
+        
+            lista[analise] = itemAnterior;
+            lista[analise - 1] = itemAnalise;
+
+            analise--;
+        }
+    }
+    console.log(lista);
+}
+insertionSort(livros);
